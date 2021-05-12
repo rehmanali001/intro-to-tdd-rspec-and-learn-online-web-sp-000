55b3eaa57ee10c05a333b072a21c8aa4dee8f706
Rehman Ali <rehmanali001@gmail.com>
2021-05-12 14:54:46 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def current_age_for_birth_year(birth_year)
   2003 - birth_year
+=======
+def current_age_for_birth_year(birth_year)
+  2003 - birth_year
+>>>>>>> 86831086509d3a8b212922ebfa28368d2dce0a18
 end
